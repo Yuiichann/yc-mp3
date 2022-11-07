9725c0bfa484773a0cf298ee82cd06ddc863e607
Yuiichan <tranduybinh3010@gmail.com>
2022-11-07 20:34:11 +0700

    check code error firebase

--- src/components/SignInWith3Party.tsx
+++ src/components/SignInWith3Party.tsx
@@ -1,9 +1,21 @@
+import { AuthError } from 'firebase/auth';
 import { memo } from 'react';
 import { useSignInWithFacebook, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { AiOutlineLoading } from 'react-icons/ai';
 import { FaFacebook } from 'react-icons/fa';
 import { FcGoogle } from 'react-icons/fc';
 import { auth } from '../config/firebase';
+import firebaseCodeError from '../constants/firebaseCodeError';
+
+// check code Error firebase
+const checkCodeErrorFirebase = (error?: AuthError) => {
+  if (!error) return '';
+  const messageAlert = Object.entries(firebaseCodeError);
+
+  const result = messageAlert.find((item) => item[0] === error?.code);
+
+  return result ? result[1] : error.message;
+};
 
 export const GoogleSignIn = memo(() => {
   // sign in with google
@@ -20,14 +32,16 @@ export const GoogleSignIn = memo(() => {
         <div className="text-20">
           {loadingGoogle ? <AiOutlineLoading className="animate-spin" /> : <FcGoogle />}
         </div>
-        <h4 className="uppercase flex-grow text-center font-semibold tracking-wide">
+        <h4 className="uppercase flex-grow text-center font-semibold tracking-wide text-12 md:text-14">
           đăng nhập với google
         </h4>
       </div>
 
       {/* Alert Error */}
       {errorGoogle && (
-        <p className="my-3 text-14 text-center text-red-600">{errorGoogle.message}</p>
+        <p className="my-3 text-14 text-center text-red-600">
+          {checkCodeErrorFirebase(errorGoogle)}
+        </p>
       )}
     </>
   );
@@ -41,6 +55,7 @@ export const FacebookSignIn = memo(() => {
   const handleSignInWithGoogle = async () => {
     await signInWithFb();
   };
+
   return (
     <>
       {/* facebook button login */}
@@ -48,13 +63,15 @@ export const FacebookSignIn = memo(() => {
         <div className="text-20">
           {loading ? <AiOutlineLoading className="animate-spin" /> : <FaFacebook />}
         </div>
-        <h4 className="uppercase flex-grow text-center font-semibold tracking-wide">
+        <h4 className="uppercase flex-grow text-center font-semibold tracking-wide text-12 md:text-14">
           đăng nhập với facebook
         </h4>
       </div>
 
       {/* Alert Error */}
-      {error && <p className="my-3 text-14 text-center text-red-600">{error.message}</p>}
+      {error && (
+        <p className="my-3 text-14 text-center text-red-600">{checkCodeErrorFirebase(error)}</p>
+      )}
     </>
   );
 });
